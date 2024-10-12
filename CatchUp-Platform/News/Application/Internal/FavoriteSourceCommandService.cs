df5c7148916a6a51b677111411e0046bfffca77d
Franco Durand <francoduve.25@gmail.com>
2024-10-11 22:58:10 -0500

    feat: implement query command service

---
+++ CatchUp-Platform/News/Application/Internal/FavoriteSourceCommandService.cs
@@ -0,0 +1,23 @@
+﻿using CatchUpPlatform.API.News.Domain.Commands;
+using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.News.Domain.Model.Queries;
+using CatchUpPlatform.API.News.Domain.Services;
+
+namespace CatchUpPlatform.API.News.Application.Services
+{
+    public class FavoriteSourceCommandService : IFavoriteSourceCommandService
+    {
+        public Task<FavoriteSource?> Handle(CreateFavoriteSourceCommand query)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<FavoriteSource?> Handle(DeleteFavoriteSourceCommand query)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<FavoriteSource?> Handle(UpdateFavoriteSourceCommand query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CatchUp-Platform/News/Application/Internal/FavoriteSourceQueryService.cs
@@ -0,0 +1,20 @@
+﻿using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.News.Domain.Model.Queries;
+using CatchUpPlatform.API.News.Domain.Services;
+
+namespace CatchUpPlatform.API.News.Application.Services
+{
+    public class FavoriteSourceQueryService : IFavoriteSourceQueryService
+    {
+
+        public Task<FavoriteSource?> Handle(GetFavoriteSourceByIdQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FavoriteSource?> Handle(GetFavoriteSourceByNewsApiKeyAndSourceIdQuery query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
